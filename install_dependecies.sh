70944f1b5a82a80acbce88fb47415f7200e13a7d
Mo Xu <xmo@cs.wisc.edu>
2025-02-17 12:12:24 -0600

    init script

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-git submodule update --init --recursive --remote        # Update submodules
+# git submodule update --init --recursive --remote        # Update submodules
 
 python3 -m pip install  -U hello-robot-stretch-urdf     # Stretch URDF
 
@@ -12,13 +12,14 @@ pip install -e .
 cd ..
 
 # Install Robosuite
-# git clone https://github.com/ARISE-Initiative/robosuite -b robocasa_v0.1
-# cd robosuite
-# pip install -e .
-# cd ../
-# git clone https://github.com/robocasa/robocasa
-# cd robocasa
-# pip install -e .
-# conda install -c numba numba -y
+cd robosuite
+pip install -e .
+cd ../
+
+# Install Robocasa
+cd robocasa
+pip install -e .
+pip install numba
+
 # python robocasa/scripts/download_kitchen_assets.py  
 # python robocasa/scripts/setup_macros.py   
